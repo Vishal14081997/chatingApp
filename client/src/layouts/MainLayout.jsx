904ba0503bd262036c3214a3511a1bb17f7871c1
Vishal14081997 <1408vishalsingh@gmail.com>
2026-06-01 13:18:29 +0530

    mainLayout  page update

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -6,7 +6,8 @@ import axios from "axios";
 import { API_BASE_URL } from "../api/config";
 // vishal singh
 const MainLayout = () => {
-    const [contacts , setContacts] = useState([])
+    const [contacts, setContacts] = useState([])
+    const [activeTab, setActiveTab] = useState("chats")
     const navigate = useNavigate();
     const token = localStorage.getItem("token")
 
@@ -33,6 +34,14 @@ const MainLayout = () => {
 
     //     },
     // ]
+    const groups = [
+        {
+            _id: "1",
+            groupIcon: "A",
+            groupName:"MyGroup",
+            participants: "1"
+        }
+    ]
     const fetchContacts = async () => {
         try {
             const res = await axios.get(`${API_BASE_URL}/api/getAllContacts`, {
@@ -46,9 +55,9 @@ const MainLayout = () => {
             console.log(error.response)
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         fetchContacts()
-    },[])
+    }, [])
     return (
         <div className="h-screen w-screen flex">
             {/* sidebar */}
@@ -72,45 +81,114 @@ const MainLayout = () => {
 
                     </div>
                 </div>
+
                 {/* Tabs */}
-                <div className="flex justify-between border-b border-gray-200">
-                    <button>
+                <div className="flex border-b justify-between border-gray-200 bg-gray-100">
+                    <button
+                        onClick={() => setActiveTab("chats")}
+                        className={`flex-1 py-2 text-sm font-medium transition
+              ${activeTab === "chats"
+                                ? "text-[#075E54] border-b-2 border-[#075E54]"
+                                : "text-gray-500"
+                            }`}
+                    >
                         Chats
                     </button>
-                    <button>
+
+                    <button
+                        onClick={() => setActiveTab("groups")}
+                        className={`flex-1 py-2 text-sm font-medium transition
+              ${activeTab === "groups"
+                                ? "text-[#075E54] border-b-2 border-[#075E54]"
+                                : "text-gray-500"
+                            }`}
+                    >
                         Groups
                     </button>
-
                 </div>
 
                 {/* contacts */}
-                <div>
-                    {
-                        contacts.map((c) => (
-                            <div 
-                            key={c._id}
-                            onClick={()=>navigate(`/chat/${c._id}`)}
-                            className="flex gap-4 mt-2 items-center"
-                            >
-                                <div className="w-12 h-12 rounded-full bg-primary text-white  flex items-center justify-center overflow-hidden">
-                                    {
-                                        c.profilePic ? (
-                                            <img src={c.profilePic} alt="" className="w-full h-full object-cover" />
-                                        ) : (
-                                            c.fullName.charAt(0)
-                                        )
-                                    }
-                                </div>
-                                <div className="">
-                                    <div className="flex justify-between items-center">
-                                        <p className="font-medium text-gray-900 text-sm">vishal singh</p>
+                <div
+                    style={{
+                        height: "calc(100vh - 160px)",
+                        overflow: "auto",
+                    }}
+                >
+                    {activeTab === "chats" ? (
+                        <>
+                            {contacts.map((c) => (
+                                <div
+                                    key={c._id}
+                                    onClick={() => navigate(`/chat/${c._id}`)}
+                                    className="flex items-center gap-3 px-4 py-3 cursor-pointer hover:bg-gray-200 border-b border-gray-100"
+                                >
+                                    <div className="relative ">
+                                        <div className="w-12 h-12 rounded-full bg-[#075E54] text-white flex items-center justify-center font-medium text-lg overflow-hidden">
+                                            {c.profilePic ? (
+                                                <img
+                                                    src={c.profilePic}
+                                                    alt=""
+                                                    className="w-full h-full object-cover"
+                                                />
+                                            ) : (
+                                                c.fullName.charAt(0)
+                                            )}
+                                        </div>
                                     </div>
-                                    <p className="text-xs text-gray-900"> online</p>
-                                </div>
-                            </div>
-                        ))
-                    }
 
+                                    <div className="">
+                                        <div className="flex justify-between items-center">
+                                            <p className="font-medium text-gray-900 text-sm">
+                                                {c.fullName}
+                                            </p>
+                                        </div>
+
+                                        <p className="text-xs text-gray-500 truncate mt-0.5">
+                                            online
+                                        </p>
+                                    </div>
+                                </div>
+                            ))}
+                        </>
+                    ) : (
+                        <>
+                            {groups.length === 0 ? (
+                                <div className="text-center py-10 text-gray-400 text-sm">
+                                    <p>No Group Found</p>
+                                </div>
+                            ) : (
+                                groups.map((g) => (
+                                    <div
+                                        key={g._id}
+                                        onClick={() => navigate(`/group/${g._id}`)}
+                                        className="flex items-center gap-3 px-4 py-3 cursor-pointer hover:bg-gray-200 border-b border-gray-100"
+                                    >
+                                        <div className="w-12 h-12 rounded-full bg-[#075E54] text-white flex items-center justify-center font-medium text-lg overflow-hidden">
+                                            {g.groupIcon ? (
+                                                <img
+                                                    src={g.groupIcon}
+                                                    alt=""
+                                                    className="w-full h-full object-cover"
+                                                />
+                                            ) : (
+                                                g.groupName.charAt(0)
+                                            )}
+                                        </div>
+
+                                        <div className="flex-1 min-w-0">
+                                            <p className="font-medium text-gray-900 text-sm">
+                                                {g.groupName}
+                                            </p>
+
+                                            <p className="text-xs text-gray-500">
+                                                {g.participants.length} members
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))
+                            )}
+                        </>
+                    )}
                 </div>
             </div>
 

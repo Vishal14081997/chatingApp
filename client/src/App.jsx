42043182066ed05debf8892ba752f0b5a904a66c
Vishal14081997 <1408vishalsingh@gmail.com>
2026-06-06 20:01:20 +0530

    socket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast"
 import MainLayout from './layouts/MainLayout'
 import Chat from './pages/Chat'
 import Group from './pages/Group'
 import Profile from './pages/Profile'
+import { io } from "socket.io-client"
+import { useEffect, useRef } from 'react'
+import { API_BASE_URL } from './api/config'
 
 const router = createBrowserRouter([
   {
@@ -22,28 +25,28 @@ const router = createBrowserRouter([
     element: <Login />
   },
   {
-    path:"/",
-    element:<MainLayout/>,
-    children:[
+    path: "/",
+    element: <MainLayout />,
+    children: [
       {
-        path:"chat",
-        element:(
+        path: "chat",
+        element: (
           <div className='flex items-center justify-center h-screen text-gray-600 font-semibold'>Select user to start chat</div>
         )
       },
       {
-        path:"chat/:userId",
-        element:<Chat/>
+        path: "chat/:userId",
+        element: <Chat />
       },
       // {
       //   path:"group/groupId",
       //   element:<Group/>
       // },
       {
-        path:"profile",
-        element:<Profile/>
+        path: "profile",
+        element: <Profile />
       },
-      
+
     ]
   },
   {
@@ -53,9 +56,26 @@ const router = createBrowserRouter([
 ])
 
 const App = () => {
+  const token = localStorage.getItem("token")
+
+  const socketRef = useRef()
+
+  useEffect(() => {
+    socketRef.current = io(`${API_BASE_URL}`, {
+      auth: { token }
+    })
+    socketRef.current.on("connect", () => {
+      console.log("connected", socketRef.current.id);
+    })
+    socketRef.current.on("onlineUser", (m) => {
+      console.log("onlineUser", m);
+    })
+
+  }, [token])
+
   return (
     <>
-      <Toaster position='top-right'/>
+      <Toaster position='top-right' />
       <RouterProvider router={router} />
     </>
   )
